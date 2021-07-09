95b4f5ce32dfe5b00f865f2bccacac5a63d20ef3
Rakesh Shrestha <rakeshshrestha3222@gmail.com>
2021-07-09 16:15:35 +0545

    kit and combos and card final

--- src/component/Kitsand combos/Kitsandcombos.js
+++ src/component/Kitsand combos/Kitsandcombos.js
@@ -18,32 +18,32 @@ const Kitsandcombos = () => {
                     <div className="container-1 container-content sale">
                         <p class="on-image">ON SALE</p>
                         <div className="kits-card">
-                            <p>PUREWAX MADNESS KIT</p>
+                            <p className="text-center">PUREWAX MADNESS KIT</p>
                             <p className="text-center"> ★★★★★(33)</p>
                             <p className="text-center">$90 &nbsp; <span className="crossed">$120</span> </p>
                         </div> 
                     </div>
                     <div className="container-2 container-content">
                         <div className="kits-card">
-                            <p>PUREWAX WATERLESS CAR </p>
-                            <p>WASH/DETAILER 1 GALLON (3.78L)</p>
-                            <p>★★★★★(73)</p>
-                            <p>$119</p>
+                            <p className="text-center">PUREWAX WATERLESS CAR </p>
+                            <p className="text-center">WASH/DETAILER 1 GALLON (3.78L)</p>
+                            <p className="text-center">★★★★★(73)</p>
+                            <p className="text-center">$119</p>
                         </div>
                     </div>
                     <div className="container-3 container-content">
                         <div className="kits-card">
-                            <p>PUREWAX: INTRO KIT</p>
-                            <p>★★★★★(71)</p>
-                            <p>$55 &nbsp; <span className="crossed">$70</span> </p>
+                            <p className="text-center">PUREWAX: INTRO KIT</p>
+                            <p className="text-center">★★★★★(71)</p>
+                            <p className="text-center">$55 &nbsp; <span className="crossed">$70</span> </p>
                         </div>
                     </div>
                     <div className="container-4 container-content">
                         <div className="kits-card">
-                            <p>WATERLESS SUPER SAVER PACKS </p>
-                            <p>(BULK COMBOS)</p>
-                            <p>★★★★★(84)</p>
-                            <p>$60 &nbsp; <span className="crossed">$90</span> </p>
+                            <p className="text-center">WATERLESS SUPER SAVER PACKS </p>
+                            <p className="text-center">(BULK COMBOS)</p>
+                            <p className="text-center">★★★★★(84)</p>
+                            <p className="text-center">$60 &nbsp; <span className="crossed">$90</span> </p>
                         </div>
                     </div>
                 </div>
